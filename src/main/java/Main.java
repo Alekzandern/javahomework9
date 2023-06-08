f509c033499c6d08a59494ee9cefc0681e06ba67
Alexzander <Alex.Zander54.ru>
2023-06-08 23:54:46 +0000

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 4444";
+        post.patronymic = "Иванович";
+        post.phone = "8-989-998-88-99";
+        post.surname = "Иванов";
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
+}
